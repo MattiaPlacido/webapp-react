c6dcec0de9105b035a0f3194b329be7e22929eb1
MattiaPlacido <placido.mattia@gmail.com>
2025-01-13 14:12:35 +0100

    Milestone 3

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-  return <p>Home</p>;
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/movies/")
+      .then((res) => res.json())
+      .then((data) => {
+        setMovieList(data);
+      });
+  }, []);
+
+  return (
+    <div className="text-center d-flex flex-column gap-3 mt-5">
+      {movieList.map((movie) => {
+        return (
+          <Link
+            className="text-decoration-none text-white"
+            to={"/movies/" + movie.id}
+          >
+            {movie.title}
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
